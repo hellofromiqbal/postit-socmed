69bb84c36c99221efdd397408e81d1b2c69e3cdc
Iqbal <hellofromiqbal@gmail.com>
2023-03-01 11:16:08 +0700

    fixed useUnlikePost custom hooks

--- src/customHooks/useUnlikePost.js
+++ src/customHooks/useUnlikePost.js
@@ -1,11 +1,14 @@
 import { collection, deleteDoc, doc, getDocs, query, where } from "firebase/firestore";
-import { database } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, database } from "../config/firebase";
 
 const useUnlikePost = () => {
+  const [user] = useAuthState(auth);
+
   const unlikePostRef = collection(database, "likes");
 
   const unlikePost = async (postId) => {
-    const queryLikeDoc = await query(unlikePostRef, where("postId", "==", postId));
+    const queryLikeDoc = await query(unlikePostRef, where("postId", "==", postId), where("userUid", "==", user?.uid));
     const querySnapshot = await getDocs(queryLikeDoc);
     const likeId = querySnapshot.docs[0].id;
     const likeToBeDeleted = doc(unlikePostRef, likeId);
